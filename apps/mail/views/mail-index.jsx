9f3da53bfc164e352d1b7804947c94fcd7f3d79d
Yarin <yarindayan11@gmail.com>
2022-08-24 11:43:07 +0300

    first commit

---
+++ apps/mail/views/mail-index.jsx
@@ -0,0 +1,15 @@
+
+export class MailIndex extends React.Component {
+
+    state = {
+        mails: []
+    }
+
+    render() {
+        return <section className="mail-index">
+
+            <h1>mail app</h1>
+           {/* <MailList mails={mails} /> */}
+        </section>
+    }
+}
---
+++ apps/note/views/note-index.jsx
@@ -0,0 +1,8 @@
+
+export class NoteIndex extends React.Component {
+    render() {
+        return (
+            <h1>note app</h1>
+        )
+    }
+}

5b49d7b025811989fb4385091c31c85a70bd6e3d
Beker <orbeker7@gmail.com>
2022-08-24 15:36:46 +0300

    Worked on the Notes

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,8 +1,20 @@
 
+import { WriteNote } from "../cmps/write-note.jsx"
+// import { NoteType } from "../cmps/note-type.jsx"
+
+
 export class NoteIndex extends React.Component {
+
+
+    
+
+
+
     render() {
-        return (
-            <h1>note app</h1>
-        )
+        return <section className="note-index">
+
+            <h1>__AppSus Notes__</h1>
+            <WriteNote />
+        </section>
     }
 }

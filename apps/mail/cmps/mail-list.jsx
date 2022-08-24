af0c1d9805be9fe2fd40720f8e76428580f99f90
Yarin <yarindayan11@gmail.com>
2022-08-24 13:17:09 +0300

    trying something

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -4,8 +4,8 @@ export function MailList({mails}){
 
     return <section className="mail-list">
         <div className="menu-logos">
-            <img className="grid-menu-logo" src="../assets/img/grid-menu.png" />
-            <img className="inbox-logo" src="../assets/img/inbox.png" />
+            <img className="grid-menu-logo" src="../../assets/img/grid-menu.png" />
+            <img className="inbox-logo" src="assets/img/inbox.png" />
             <img className="star-logo" src="../assets/img/star-logo.png" />
             <img className="new-mail-logo" src="../assets/img/new-mail-logo.png" />
             <img className="sent-logo" src="../assets/img/sent-logo.png" />

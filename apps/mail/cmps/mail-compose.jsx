129ed9ca3c13f35fbcc01d6cdb038edc38f4ffd6
Yarin <yarindayan11@gmail.com>
2022-08-25 20:55:53 +0300

    added the option to filter + add new mail

---
+++ apps/mail/cmps/mail-compose.jsx
@@ -0,0 +1,119 @@
+import { utilService } from "../../../services/util.service.js"
+
+
+export class MailCompose extends React.Component {
+
+    state = {
+        newMail: {
+            subject: '',
+            body: '',
+            isRead: false,
+            sentAt: null,
+            to: '',
+            isSentToTrash: false,
+            isStared: false
+        },
+        draftMails: []
+    }
+
+    intervalId = null
+
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState({ newMail: this.state.newMail }, console.log(this.state.newMail))
+        }, 5000)
+    }
+
+    componentWillUnmount() {
+        const { newMail } = this.state
+        if (!newMail.sentAt) console.log('Hey');
+        clearInterval(this.intervalId)
+    }
+
+    resetMailData = () => {
+        this.setState({
+            newMail: {
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: null,
+                to: '',
+                isSentToTrash: false,
+                isStared: false
+            }
+        })
+    }
+
+    onChangeNewMail = (ev) => {
+        ev.preventDefault()
+        const { newMail } = this.state
+        if (newMail.to === '') return
+        // if (newMail.body === '') {
+        //     this.setState((prevState) => ({
+        //         newMail: {
+        //             ...prevState.newMail,
+        //             body: 'No Body Msg'
+        //         }
+        //     }))
+        // }
+        // if (newMail.subject === '') {
+        //     this.setState((prevState) => ({
+        //         newMail: {
+        //             ...prevState.newMail,
+        //             subject: 'No subject Msg'
+        //         }
+        //     }))
+        // }
+            this.props.onAddMail(newMail, ev)
+        this.resetMailData()
+    }
+
+    handleChange = ({ target }) => {
+        const field = target.name
+        const value = target.value
+        this.setState((prevState) => ({
+            newMail: {
+                ...prevState.newMail,
+                [field]: value
+            }
+        }))
+    }
+
+    render() {
+        // const { newMail } = this.state
+        const { to, subject, body } = this.state.newMail
+        return <section className="mail-compose-container">
+            <div className="mail-compose-header">New Message</div>
+            <form className="mail-compose-form" onSubmit={(ev) => this.onChangeNewMail(ev)}>
+                <label htmlFor="to"></label>
+                <input
+                    type="text"
+                    placeholder="addressee"
+                    id="to"
+                    name="to"
+                    value={to}
+                    onChange={this.handleChange}
+                />
+                <label htmlFor="subject"></label>
+                <input
+                    type="text"
+                    placeholder="Subject"
+                    id="subject"
+                    name="subject"
+                    value={subject}
+                    onChange={this.handleChange}
+                />
+                <label htmlFor="body"></label>
+                <textarea
+                    type="text"
+                    placeholder=""
+                    id="new-mail-body"
+                    name="body"
+                    value={body}
+                    onChange={this.handleChange}
+                ></textarea>
+                <div className="new-mail-tools"><button>Send</button></div>
+            </form>
+        </section>
+    }
+}
